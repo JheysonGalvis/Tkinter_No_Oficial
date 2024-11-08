408ebb85ca9ade2e82d44cb9d3dfe35ab1e84f4d
JheysonGalvis <tecnojheyson@gmail.com>
2024-11-08 11:00:01 -0500

    Creamos una clase para el programa

--- 01-ventana.py
+++ 01-ventana.py
@@ -1,25 +1,48 @@
-# Tkinter
-# Es un módulo para crear interfaces gráficas de usuario.
-
-# Haremos from tkinter import * para importar todo el módulo.
 from tkinter import *
 
-# Crearemos la ventana principal.
+class Programa:
+    def __init__(self):
+        self.title = "Aprendiendo Tkinter"
+        self.icon = "images/free-30-instagram-stories-icons53_122600.ico"
+        self.size = "770x470"
+        self.resizable = False
+        
+    def cargar(self):
+        # Crearemos la ventana principal.
+        ventana = Tk()  # El objeto TK() es el que nos permite crear la ventana.
+        self.ventana = ventana
 
-ventana = Tk() # El objeto TK() es el que nos permite crear la ventana.
+        # Título de la ventana
+        ventana.title(self.title)
 
-# Título de la ventana
-ventana.title("Interfaz gráfica con Python y Tkinter")
+        # Icono de la ventana
+        ventana.iconbitmap(self.icon)
 
-# Icono de la ventana
-ventana.iconbitmap("images/free-30-instagram-stories-icons53_122600.ico")
+        # Cambio en el tamaño de la ventana
+        ventana.geometry(self.size)
 
-# Cambio en el tamaño de la ventana
-ventana.geometry("750x450")
+        # Bloquear el tamaño de la ventana
+        if self.resizable:
+            ventana.resizable(1, 1)
+        else:
+            ventana.resizable(0, 0)
 
-# Bloquear el tamaño de la ventana
-ventana.resizable(0,0)
+    def addTexto(self, dato):
+        # Añadir un texto a la ventana
+        texto = Label(self.ventana, text=dato)
+        texto.pack()
 
-# Arrancar y mostrar la venana hasta que se cierre
+    def mostrar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
 
-ventana.mainloop()
+# Instanciar y ejecutar el programa
+programa = Programa()
+programa.cargar()
+programa.addTexto("Hola")
+programa.addTexto("Soy un texto")
+programa.addTexto("Bienvenidos")
+programa.addTexto("Aprendiendo Tkinter")
+programa.addTexto("Con Python")
+programa.addTexto("Nunca pares de aprender")
+programa.mostrar()
